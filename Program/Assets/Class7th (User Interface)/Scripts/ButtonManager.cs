acde38fa9173b2791af63bf64a77a2e4f9ea450f
STH <sth6197@gmail.com>
2024-08-19 19:45:41 +0900

    (2024.08.19) 유저 인터페이스

---
+++ Program/Assets/Class7th (User Interface)/Scripts/ButtonManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    [SerializeField] Text[] buttonTexts;
+    [SerializeField] string[] buttonNames;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
