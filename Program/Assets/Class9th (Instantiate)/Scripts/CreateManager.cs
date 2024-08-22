ebc40bc33fb5060a2cf669d75cc09e0d04cf4525
STH <sth6197@gmail.com>
2024-08-22 19:46:43 +0900

    (2024.08.22) 이벤트 트리거

--- Program/Assets/Class9th (Instantiate)/Scripts/CreateManager.cs
+++ Program/Assets/Class9th (Instantiate)/Scripts/CreateManager.cs
@@ -1,21 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CreateManager : MonoBehaviour
 {
-    [SerializeField] GameObject food;
+    [SerializeField] int count = 0;
+
+    [SerializeField] GameObject [] foods;
+    
     [SerializeField] GameObject clone;
 
     [SerializeField] float time;
+
+    WaitForSeconds waitForSeconds = new WaitForSeconds(3.0f);
+
+    private void Start()
+    {
+        StartCoroutine(Create());
+    }
+
+    IEnumerator Create()
+    {
+        while (count < foods.Length)
+        {
+            if (clone == null)
+            {
+                clone = Instantiate(foods[count++]);
+            }
+            else if (clone != null)
+            {
+                yield return waitForSeconds;
+
+                for (int i = 0; i < foods.Length; i++)
+                {
+                    clone = Instantiate(foods[i]);
+
+                    clone.transform.position = new Vector3(Random.Range(0, 101), Random.Range(0, 301));
+                }
+            }
+        }
+            yield return waitForSeconds;
+    }
 
     void Update()
     {
         time += Time.deltaTime;
 
-        if(time >= 5.0f)
+        if(time >= 3.0f)
         {
-            clone = Instantiate(food);
+            clone = Instantiate(foods[0]);
+
 
             time = 0.0f;
         }
