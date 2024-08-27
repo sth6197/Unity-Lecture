e293ff1210806d76735db45c6d91c5f5ba535ab3
STH <sth6197@gmail.com>
2024-08-27 19:46:40 +0900

    (2024.08.27) 애니메이션 이벤트

---
+++ Program/Assets/Class11th (Sound)/Scripts/Dragon.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dragon : MonoBehaviour
+{
+    [SerializeField] AudioClip audioClip;
+    
+    public void Attack()
+    {
+        SoundManager.instance.Listen(audioClip);
+    }
+
+    
+}
---
+++ Program/Assets/Class11th (Sound)/Scripts/SoundManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+
+    [SerializeField] AudioSource effectAudioSource;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        effectAudioSource = GetComponentInChildren<AudioSource>();
+    }
+
+    public void Listen(AudioClip audioClip)
+    {
+        effectAudioSource.PlayOneShot(audioClip);
+    }
+
+}
