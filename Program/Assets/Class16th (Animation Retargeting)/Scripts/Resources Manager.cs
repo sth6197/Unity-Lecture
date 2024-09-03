f8cb441cc548359b5935c166ded318777d84550e
STH <sth6197@gmail.com>
2024-09-03 19:45:12 +0900

    (2024.09.03) 애니메이션 리타겟팅

---
+++ Program/Assets/Class16th (Animation Retargeting)/Scripts/Resources Manager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourcesManager : MonoBehaviour
+{
+    [SerializeField] string[] names;
+
+    private void Awake()
+    {
+        for (int i = 0; i < names.Length; i++)
+        {
+            Instantiate(Resources.Load<GameObject>(names[i]));
+        }
+    }
+}
